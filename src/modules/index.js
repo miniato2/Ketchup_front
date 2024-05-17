ba56314aad4a4ffde739413283db6e7f69137032
KIMHYEONJI13 <davithjk1310@gmail.com>
2024-05-17 15:48:52 +0900

    .

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,13 +5,9 @@ import mailReducer from "./MailModule";
 import memberReducer from "./MemberModule";
 import scheduleReducer from "./ScheduleModule";
 import reserveReducer from "./ReserveModule";
-<<<<<<< HEAD
-import boardReducer from "./BoardModule";
-=======
 import resourceReducer from "./ResourceModule";
 import departmentReducer from "./DepartmentModule";
 import positionReducer from "./PositionModule";
->>>>>>> 07317f8d48a7e6ff9261cf94bf0a6f05904c5140
 
 const rootReducer = combineReducers({
     // userReducer,
@@ -22,14 +18,10 @@ const rootReducer = combineReducers({
     approvalReducer,
     mailReducer,
     reserveReducer,
-<<<<<<< HEAD
-    boardReducer
-=======
     resourceReducer,
     departmentReducer,
     positionReducer,
 
->>>>>>> 07317f8d48a7e6ff9261cf94bf0a6f05904c5140
 });
 
 export default rootReducer;
