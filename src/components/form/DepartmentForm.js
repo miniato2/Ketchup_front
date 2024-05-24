95322ab9270763d0cd86c681821f189fecd8c152
2eehy <yihooyung@gmail.com>
2024-05-24 17:45:44 +0900

    commit for pull

--- src/components/form/DepartmentForm.js
+++ src/components/form/DepartmentForm.js
@@ -1,45 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { callAllPositionsAPI, callUpdatePositionAPI } from '../../apis/MemberAPICalls';
+import { callAllDepartmentsAPI,callUpdateDepartmentAPI } from '../../apis/MemberAPICalls';
 import { useDispatch } from 'react-redux';
+import ButtonGroup from '../contents/ButtonGroup';
 
 
 
 export default function DepartmentForm({ department, onDialogClickHandler }) {
 
+    console.log('다이얼로그 시작 수정할 부서', department);
     const dispatch = useDispatch();
-    const [form, setForm] = useState([]);
-    console.log(form);
+    const [form, setForm] = useState('');
 
-    const positionName = position.positionName;
 
-    useEffect(() => {
-        setForm({
-            positionName: position.positionName,
-            positionLevel: position.positionLevel,
-            authority: position.authority,
-            positionStatus: position.positionStatus
-        });
-    }, []);
+   
 
 
     const onChangeHandler = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value,
-        });
+        setForm(
+           e.target.value,
+        );
     }
 
-    const onClickHandler =async () => {
-       await dispatch(callUpdatePositionAPI(position.positionNo,form));
-      
+    const onClickHandler = async () => {
+        await dispatch(callUpdateDepartmentAPI(department.depNo, form));
+
         alert("수정이 완료되었습니다.");
 
-       await dispatch(callAllPositionsAPI());
+        await dispatch(callAllDepartmentsAPI());
 
-       onDialogClickHandler();
+        onDialogClickHandler();
     }
 
-    
+
 
 
 
@@ -48,46 +40,42 @@ export default function DepartmentForm({ department, onDialogClickHandler }) {
     return (
 
 
-        <div style={{ marginLeft: 50 }}>
-            <label>현재 직급명 <input value={positionName} style={{ textAlign: 'center', marginLeft: 50, borderWidth: '0px 0px 1px 0px' }}></input></label>
+        <div style={{ marginLeft: 50, width: 500}}>
+            <label>현재 부서명 <input value={department.depName} style={{ textAlign: 'center', marginLeft: 50, borderWidth: '0px 0px 1px 0px' }}></input></label>
             <br></br>
 
 
-            <label>수정후 직급레벨 <select style={{ marginLeft: 20, borderWidth: '0px 0px 1px 0px' }} name="positionLevel" onChange={onChangeHandler}>
-                <option value="">직급 레벨 선택</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select></label>
 
 
-            <label>수정후 직급명
+
+            <label>수정후 부서명
                 <input
-                    defaultValue={positionName}
+                    defaultValue={department.depName}
                     onChange={onChangeHandler}
-                    name="positionName"
+                    name="depName"
                     style={{
                         textAlign: 'center',
-                        marginLeft: 35,
+                        marginLeft: 40,
                         borderWidth: '0px 0px 1px 0px'
                     }}>
                 </input>
             </label>
 
 
-            <label>수정후 관리권한
-                <select style={{ marginLeft: 20, borderWidth: '0px 0px 1px 0px' }} name="authority" onChange={onChangeHandler}>
-                    <option value="">관리 권한 선택</option>
-                    <option value="LV1">LV1</option>
-                    <option value="LV2">LV2</option>
-                    <option value="LV3">LV3</option>
-                </select></label>
+
 
             <br></br>
-            <button style={{ marginLeft: 330, marginBottom: 20}} onClick={()=>{onDialogClickHandler()}}>취소</button>
-            <button style={{ marginLeft: 5 }} onClick={onClickHandler}>저장</button>
+            {/* <button style={{ marginLeft: 330, marginBottom: 20 }} onClick={() => { onDialogClickHandler() }}>취소</button>
+            <button style={{ marginLeft: 5 }} onClick={onClickHandler}>저장</button> */}
+
+<div style={{ marginRight: "20px"}}>
+                <ButtonGroup
+                    buttons={[
+                        { label: '취소', styleClass: 'back', onClick: () => { onDialogClickHandler() } },
+                        { label: '저장', styleClass: 'move', onClick: onClickHandler }
+                    ]}
+                />
+            </div>
 
 
 
