8ac9b6bb0cc5a462217e9ed7fb505736244a7a45
jaylee <jayginwoolee98@gmail.com>
2024-05-29 03:09:28 +0900

    일정 필터링 기능 추가

--- src/components/form/ScheduleForm.js
+++ src/components/form/ScheduleForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Grid, TextField, Box, Button, ListItem, ListItemText, Typography, DialogContent } from "@mui/material";
+import { Grid, TextField, Box, Typography, DialogContent, Select, MenuItem, InputLabel, FormControl, Radio, RadioGroup, FormControlLabel } from "@mui/material";
 import CampaignIconOutlined from '@mui/icons-material/CampaignOutlined';
 import CalendarMonthOutlined from '@mui/icons-material/CalendarMonthOutlined';
 import LocationOnOutlined from '@mui/icons-material/LocationOnOutlined';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined'
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import ButtonGroup from '../contents/ButtonGroup';
 
-export default function ScheduleForm({ newScheduleData, onInsertCancelHandler, handleSubmit, handleInputChange, skdNameError, setTouched, dateError, touched }) {
+export default function ScheduleForm({ newScheduleData, onInsertCancelHandler, handleSubmit, handleInputChange, skdNameError, setTouched, dateError, touched, memberList, handleParticipantsChange, dptNo }) {
+    const filteredMemberList = memberList.filter(member => member.department.depNo === dptNo);
     const buttons = [
         { label: '취소', onClick: onInsertCancelHandler, styleClass: 'back' },
         { label: '등록', onClick: () => handleSubmit(newScheduleData), styleClass: 'move' }
@@ -57,6 +59,62 @@ export default function ScheduleForm({ newScheduleData, onInsertCancelHandler, h
                         </Box>
                     </Grid>
                     <Grid item xs={12}>
+                        <Box display="flex" alignItems={'center'} justifyContent="flex-start">
+                            <Box display="flex" alignItems="center">
+                                <PersonOutlinedIcon fontSize="medium" />
+                                <Typography variant="body1" sx={{ ml: 1 }} flexShrink={0}>일정 등록자:</Typography>
+                            </Box>
+                            <Typography variant="body1" sx={{ ml: 6.3, width: '50vw' }}>{newScheduleData.authorName}</Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl>
+                            <Box display="flex" alignItems={'center'} justifyContent="flex-start">
+                                <Box display="flex" alignItems="center">
+                                    <PersonOutlinedIcon fontSize="medium" />
+                                    <Typography variant="body1" sx={{ ml: 1 }} flexShrink={0}>참여 인원:</Typography>
+                                </Box>
+                                <Select
+                                    multiple
+                                    value={newScheduleData.participants?.map(participant => participant.participantMemberNo) || []}
+                                    onChange={handleParticipantsChange}
+                                    renderValue={(selected) => {
+                                        const selectedNames = selected?.map(memberNo => {
+                                            const member = filteredMemberList?.find(member => member.memberNo === memberNo);
+                                            return member ? member.memberName : '';
+                                        }).filter(name => name !== '');
+
+                                        return selectedNames.join(', ');
+                                    }}
+                                    sx={{ width: '50vw', ml: 1.7 }}
+                                >
+                                    {filteredMemberList.map((member) => (
+                                        <MenuItem key={member.memberNo} value={member.memberNo}>
+                                            {member.memberName}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </Box>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Box display="flex" alignItems={'center'} justifyContent="flex-start">
+                            <Box display="flex" alignItems="center">
+                                <ArticleOutlinedIcon fontSize="medium" />
+                                <Typography variant="body1" sx={{ ml: 1 }} flexShrink={0}>일정 상태:</Typography>
+                            </Box>
+                            <FormControl component="fieldset" sx={{ ml: 1.7 }}>
+                                <RadioGroup row aria-label="status" name="skdStatus" value={newScheduleData.skdStatus} onChange={handleInputChange}>
+                                    <FormControlLabel value="예정" control={<Radio />} label="예정" />
+                                    <FormControlLabel value="진행 중" control={<Radio />} label="진행 중" />
+                                    <FormControlLabel value="완료" control={<Radio />} label="완료" />
+                                    <FormControlLabel value="보류" control={<Radio />} label="보류" />
+                                    <FormControlLabel value="막힘" control={<Radio />} label="막힘" />
+                                </RadioGroup>
+                            </FormControl>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
                         <Box justifyContent={'flex-end'}>
                             <Box>
                                 <ButtonGroup buttons={buttons} />
@@ -65,6 +123,6 @@ export default function ScheduleForm({ newScheduleData, onInsertCancelHandler, h
                     </Grid>
                 </Grid>
             </Box>
-        </DialogContent>
+        </DialogContent >
     );
 }
---
+++ src/components/lists/subscriptions/SubscriptionList.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Table } from 'react-bootstrap';
+import SearchBar from '../../contents/SearchBar';
+import { callPageMembersAPI } from '../../../apis/MemberAPICalls';
+import PaginationButtons from '../../contents/PaginationButtons';
+import Checkbox from '@mui/material/Checkbox';
+import { red } from '@mui/material/colors';
+import { Box, Grid } from '@mui/material';
+import { decodeJwt } from '../../../utils/tokenUtils';
+
+const SubscriptionList = ({ subscribedMembers, setSubscribedMembers }) => {
+    const dispatch = useDispatch();
+    const token = decodeJwt(window.localStorage.getItem("accessToken"));
+    const dptNo = token?.depNo;
+    const schedules = useSelector(state => state.scheduleReducer);
+    const scheduleList = schedules?.results?.schedule || [];
+    const members = useSelector(state => state.memberReducer);
+    const memberList = members?.data?.content || [];
+    const filteredMemberList = memberList.filter(member => member.department.depNo === dptNo);
+    const itemsPerPage = 10;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchKeyword, setSearchKeyword] = useState('');
+
+    useEffect(() => {
+        const initialSubscribedMembers = filteredMemberList.map(member => member.memberNo);
+        setSubscribedMembers(initialSubscribedMembers);
+    }, []);
+
+    useEffect(() => {
+        dispatch(callPageMembersAPI(currentPage, searchKeyword));
+    }, [dispatch, currentPage, searchKeyword]);
+
+    const handleSearch = (searchKeyword) => {
+        setSearchKeyword(searchKeyword);
+        dispatch(callPageMembersAPI(currentPage, searchKeyword));
+    };
+
+    const handleSubscribeChange = (memberNo) => {
+        if (subscribedMembers.includes(memberNo)) {
+            setSubscribedMembers(subscribedMembers.filter(id => id !== memberNo));
+        } else {
+            setSubscribedMembers([...subscribedMembers, memberNo]);
+        }
+    };
+
+    return (
+        <Box p={3} mt={7} display={'flex'} width={'1000px'}>
+            <Grid container spacing={3}>
+                <Grid item lg={12}>
+                    <div>
+                        <h5>다가오는 일정</h5>
+                    </div>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>날짜</th>
+                                <th>일정 제목</th>
+                                <th>작성자</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {scheduleList.slice(0, 5).map((schedule) => (
+                                <tr key={schedule.skdNo}>
+                                    <td>{schedule.skdStartDttm}</td>
+                                    <td>{schedule.skdName}</td>
+                                    <td>{schedule.authorName}</td>
+                                </tr>
+                            ))}
+                            {scheduleList.length === 0 &&
+                                <td colSpan={6}>
+                                    <h6>일정 목록이 비어있습니다.</h6>
+                                </td>
+                            }
+                        </tbody>
+                    </Table>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item md={12} xs={6} lg={12}>
+                    <div>
+                        <h5>일정 구독</h5>
+                        <SearchBar onSearch={handleSearch} value={searchKeyword} name={'이름으로 검색'} />
+                    </div>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>구독</th>
+                                <th>프로필 사진</th>
+                                <th>직급</th>
+                                <th>이름</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredMemberList.map((member) => (
+                                <tr key={member.memberNo}>
+                                    <td>
+                                        <Checkbox
+                                            checked={subscribedMembers.includes(member.memberNo)}
+                                            onChange={() => handleSubscribeChange(member.memberNo)}
+                                            sx={{ color: red[800], '&.Mui-checked': { color: red[600] } }}
+                                        />
+                                    </td>
+                                    <td>
+                                        <img src={`/img/${member.imgUrl}`} width={30} height={30} alt='memberImg' />
+                                    </td>
+                                    <td>{member.position.positionName}</td>
+                                    <td>{member.memberName}</td>
+                                </tr>
+                            ))}
+                            {filteredMemberList.length === 0 && (
+                                <tr>
+                                    <td colSpan={6}>
+                                        <h6>검색 결과가 없습니다.</h6>
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table>
+                    <PaginationButtons
+                        totalItems={members?.data?.totalElements}
+                        itemsPerPage={itemsPerPage}
+                        currentPage={currentPage}
+                        onPageChange={(pageNumber) => setCurrentPage(pageNumber)}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default SubscriptionList;
